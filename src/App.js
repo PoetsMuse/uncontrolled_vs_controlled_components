0c27ed82084401065a266c9367f8594566104c98
Kate Glushkova <weirdis1@gmail.com>
2023-11-13 12:19:35 +0200

    Uncontrolled onboarding flows

--- src/App.js
+++ src/App.js
@@ -3,6 +3,27 @@ import { UncontrolledForm } from "./UncontrolledForm";
 import { ControlledForm } from "./ControlledForm";
 import { UncontrolledModal } from "./UncontrolledModal";
 import { ControlledModal } from "./ControlledModal";
+import { UncontrolledOnboardingFlow } from "./UncontrolledOnboardingFlow";
+
+const StepOne = ({goToNext}) => (
+  <>
+    <h1>Step 1</h1>
+    <button onClick={goToNext}>Next</button>
+  </>
+);
+const StepTwo = ({goToNext}) => (
+  <>
+    <h1>Step 2</h1>
+    <button onClick={goToNext}>Next</button>
+  </>
+);
+const StepThree = ({goToNext}) => (
+  <>
+    <h1>Step 3</h1>
+    <button onClick={goToNext}>Next</button>
+  </>
+);
+
 
 function App() {
   const [shouldShowModal, setShouldShowModal] = useState(false);
@@ -19,6 +40,12 @@ function App() {
           <h1>Hello!</h1>
       </ControlledModal>
       <button onClick={() => setShouldShowModal(!shouldShowModal)}>{shouldShowModal ? 'Hide Controlled Modal' : 'Show Controlled Modal'}</button>
+
+      <UncontrolledOnboardingFlow>
+        <StepOne />
+        <StepTwo />
+        <StepThree />
+      </UncontrolledOnboardingFlow>
     </>
   );
 }
